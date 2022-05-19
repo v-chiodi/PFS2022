48a4d251d5ffd51ea67166123a418b78d0c6e7ca
v-chiodi <105468018+v-chiodi@users.noreply.github.com>
2022-05-19 20:03:34 -0300

    Delete dados.js

--- 1-PB/Clase_3/dados.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var readlinesync = require("readline-sync");
-console.log("BUENOS DIAS, INGRESE LA CANT DE DADOS");
-var cantidad = readlinesync.questionInt();
-var denominador = (Math.pow(6, cantidad));
-var probabilidad = (1 / denominador);
-console.log("LAS PROBABILIDADES DE SACAR TODOS LOS DADOS IGUALES ES DE: " + probabilidad);
