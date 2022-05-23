7a0bf8d00c20b98fbda78f53583604627a8eb3cb
v-chiodi <chiodi290@gmail.com>
2022-05-22 23:05:08 -0300

    main

---
+++ 1-PB/Clase_4/potencias.ts
@@ -0,0 +1,10 @@
+import* as readlinesync from 'readline-sync'
+console.log("EN ESTE PROGRAMA, AL INGRESAR EL NUMERO DE LA BASE, Y EL NNUMERO DE LA POTENCIA, HALLARAS EL RESULTADO")
+
+let number1 : number = readlinesync.questionInt ('Ingrese el numero de la base')
+console.log("EL NUMERO DE LA BASE INGRESADO FUE " + number1)
+let number2 : number = readlinesync.questionInt('Ingrese el numero del exponente, este debe ser 0 =<')
+console.log("EL EXP ES "+ number2)
+
+let RESULTADO:number = number1**number2
+console.log ("EL RESULTADO ES " + RESULTADO)
