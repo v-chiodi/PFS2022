dcfb849c67d2472638ef4bee9efe4d433d7ce63f
Mona Lisa <chiodi290@gmail.com>
2022-05-29 15:45:51 -0300

    main

--- 1-PB/Clase_5/invertirArreglo.ts
+++ 1-PB/Clase_5/invertirArreglo.ts
@@ -1,8 +1,14 @@
 import * as readlinesync from 'readline-sync'
+
+
 let nArreglo : number = readlinesync.questionInt ("INGRESE LA DIMENSION DE SU ARREGLO: " )
-let arreglo1 []: number = new Array (nArreglo)
+let arreglo1 : number [] = new Array (nArreglo)
+let arregloInvertido : number [] = new Array
+
+
 let contador = 0
-for (contador=0;contador=nArreglo;contador++){
-    let nArreglo = readlinesync.questionFloat("INGRESE LOS VALORES DEL ARREGLO")
+for (contador=0;contador < nArreglo;contador++){
+     arreglo1 [contador] = readlinesync.questionFloat("INGRESE LOS VALORES DEL ARREGLO: ");
+    arregloInvertido [nArreglo-1-contador]=arreglo1[contador]
 }
-console.log("Los valores del arreglo son: " + arreglo1 + " a la inversa, seria: " + (arreglo1));
+console.log("Los valores del arreglo son: " + (arreglo1) + " a la inversa, seria: " + (arregloInvertido));
